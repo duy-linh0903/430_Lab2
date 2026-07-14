f61e8096bf291896395181eadea338faff9ea9e5
Duy Linh <linh.nguyendinh.cit23@eiu.edu.vn>
2026-07-15 00:29:35 +0700

    Add container to complete design Login Screen

--- Q3_Login/Login.js
+++ Q3_Login/Login.js
@@ -1,20 +1,20 @@
 import { ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
-import styles from "./style";
+import Styles from "./style";
 
 const LoginScreen = () => {
     return (
-        <ScrollView showsVerticalScrollIndicator={false}>
-            <View style={styles.container}>
-                <Text style={styles.title}>Login</Text>
+        <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={Styles.container}>
+            <View style={Styles.container}>
+                <Text style={Styles.title}>Login</Text>
                 <TextInput
                     placeholder="Phone"
-                    style={styles.input}/>
+                    style={Styles.input}/>
                 <TextInput
                     placeholder="Password"
-                    style={styles.input}
+                    style={Styles.input}
                     secureTextEntry/>
-                <TouchableOpacity style={styles.btn}>
-                    <Text style={styles.btntext}>Login</Text>
+                <TouchableOpacity style={Styles.btn}>
+                    <Text style={Styles.btntext}>Login</Text>
                 </TouchableOpacity>
             </View>
         </ScrollView>
